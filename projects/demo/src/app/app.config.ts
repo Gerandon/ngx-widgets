293d472796c2b0d4614428b99611460d8e9d2336
Gergő Asztalos <gergo0317@gmail.com>
2025-06-05 14:07:25 +0200

    Angular20
    * ng update @angular/core@20 @angular/cli@20
    
    * ng update @angular/material@20
    
    * appearance modifications
    
    * npm dedupe
    
    * readme update
    
    ---------
    
    Co-authored-by: Gergő Asztalos <gergo.asztalos@4ig.hu>

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {provideEnvironmentNgxMask} from "ngx-mask";
-import {NGX_WIDGETS_VALIDATION_TRANSLATIONS} from "@gerandon/ngx-widgets";
+import {NGX_WIDGETS_VALIDATION_TRANSLATIONS, NGX_WIDGETS_FORM_FIELD_APPEARANCE} from "@gerandon/ngx-widgets";
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -17,6 +17,10 @@ export const appConfig: ApplicationConfig = {
         required: 'This field is required!',
         selectGlobalPlaceholder: 'Please choose one from the existing options!'
       }
+    },
+    {
+      provide: NGX_WIDGETS_FORM_FIELD_APPEARANCE,
+      useValue: 'fill'
     }
   ]
 };
